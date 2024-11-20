a32e1b67fc278d10408c6e3fb547f6ce5f9850d7
augustus mocha <mocha4pf@gmail.com>
2024-11-20 10:16:18 +0300

    final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,21 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // Map over the links array to create an <a> tag for each link
+  const navBarLinks = links.map((link) => {
+    return (
+      <a key={link} href={`#${link}`}>
+        {link}
+      </a>
+    );
+  });
+
+  // Render the navigation bar with the list of links
+  return (
+    <nav>
+      {navBarLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
